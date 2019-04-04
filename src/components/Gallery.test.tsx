a7a3dcaa3809a95e5cdae3b4bd16f08dea26a327
Martin Trenker <martin@martintrenker.de>
2019-04-04 13:28:45 +0200

    100% CC the "free" way

--- src/components/Gallery.test.tsx
+++ src/components/Gallery.test.tsx
@@ -1,11 +1,38 @@
 import * as React from 'react';
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 
-import { Gallery } from './Gallery';
+import { Gallery, Slider, ISlider, Modal } from './Gallery';
 
 describe('Gallery', () => {
-  it('Exists', () => {
-    const wrapper = shallow(<Gallery />);
-    expect(wrapper.text()).toBe('Gallery');
+  it('displays a Gallery with 3 Images', () => {
+    const images = [{
+      src: 'https://source.unsplash.com/800x600/?alpaca'
+    },{
+      src: 'https://source.unsplash.com/800x600/?alpaca'
+    },{
+      src: 'https://source.unsplash.com/800x600/?alpaca'
+    }];
+    const wrapper = mount(<Gallery images={images} />);
+    expect(wrapper.find('img').length).toBe(3);
+  })
+
+  it('displays the Slider', () => {
+    const sliderProps: ISlider = {
+      image: 'https://source.unsplash.com/800x600/?alpaca',
+      currentIndex: 1,
+      imageCount: 10,
+    };
+    const wrapper = mount(<Slider {...sliderProps} />);
+    expect(wrapper.find('p').text()).toBe('2 / 10');
+  })
+
+  it('displays the Modal', () => {
+    const sliderProps: ISlider = {
+      image: 'https://source.unsplash.com/800x600/?alpaca',
+      currentIndex: 1,
+      imageCount: 10,
+    };
+    const wrapper = mount(<Modal open={true}><Slider {...sliderProps} /></Modal>);
+    expect(wrapper.find('RawCloseButton button').text()).toBe('Close');
   })
 })
