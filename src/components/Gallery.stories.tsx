47b071fbdb923910f8fb686fabb9dfb81a5f1a9c
Martin Trenker <martin@martintrenker.de>
2019-04-04 10:06:46 +0200

    Add storybook

---
+++ src/components/Gallery.stories.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import { Gallery } from './Gallery';
+
+storiesOf('Gallery', module)
+  .add('default', () => (
+    <Gallery />
+  ));
